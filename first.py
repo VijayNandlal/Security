3d80243f4a2a4c080765d744f39fa438f78c0d2e
Vijay <vijaynandlal@outlook.com>
2021-05-07 18:47:07 -0400

    getting somewhere

--- first.py
+++ first.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -9,7 +12,15 @@ while(True):
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+    for (x, y, w, z) in faces:
+        print(x, y, w, z)
+
+
+    if cv2.waitKey(20) & 0xFF == ord(' '):
         break
 
 # When everything done, release the capture
