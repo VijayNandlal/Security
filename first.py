e321e1481939fd616a5adf0234abd6aa5189db0a
VijayNandlal <vijaynandlal@icloud.com>
2021-05-07 20:17:40 -0400

    t

--- first.py
+++ first.py
@@ -39,7 +39,7 @@ while(True):
         frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 2), 3)
 
     for (x, y, w, h) in eyeBallFind:
-        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 2), 3
+        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 2), 3)
 
     for (x, y, w, h) in fullBodyGet:
         frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 2), 3)
