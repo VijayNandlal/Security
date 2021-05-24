03cedbc647a78d6695a7bdbf5c6095b76d115612
VijayNandlal <71157452+VijayNandlal@users.noreply.github.com>
2021-05-24 17:22:51 -0400

    Update Runtime.java

--- src/sample/Runtime.java
+++ src/sample/Runtime.java
@@ -1,12 +1,13 @@
 package sample;
 
-import javafx.scene.image.Image;
 import org.opencv.core.*;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.*;
 import org.opencv.imgproc.*;
+import java.time.LocalTime;
 
-import java.util.ArrayList;
 import java.util.List;
 import javafx.scene.image.ImageView;
 import static org.opencv.highgui.HighGui.imshow;
@@ -21,7 +22,10 @@ public class Runtime {
 
         Mat frame = new Mat();
 
-        CascadeClassifier face_detect = new CascadeClassifier("C:\\OpenCv\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml");
+        CascadeClassifier face_detect = new CascadeClassifier("src/cascade/haarcascade_frontalface_default.xml");
+        CascadeClassifier upper_body = new CascadeClassifier("src/cascade/haarcascade_upperbody.xml");
+
+        LocalTime time = LocalTime.now();
 
         while(true){
             if(!cap.isOpened()){
@@ -41,19 +45,38 @@ public class Runtime {
                 return 1;
             }
 
-            MatOfRect holder = new MatOfRect();
-            face_detect.detectMultiScale(grey_image, holder, 1.2, 10);
+            MatOfRect face_holder = new MatOfRect();
+            MatOfRect upper_holder = new MatOfRect();
 
-            List<Rect> hol = holder.toList();
-            for(Integer i = 0 ; i < hol.size(); i++){
+            face_detect.detectMultiScale(grey_image, face_holder, 1.2, 3);
+            upper_body.detectMultiScale(grey_image, upper_holder, 1.2, 3);
+
+            int flag = 1;
+
+            List<Rect> hol = face_holder.toList();
+            List<Rect> up_hol = upper_holder.toList();
+
+            if (hol.size() == 0 && up_hol.size() == 0){
+                flag = 0;
+            }
+
+            for(int i = 0 ; i < hol.size(); i++){
                 rectangle(frame, hol.get(i), new Scalar(255, 255, 0), 4);
+            }
+
+            for(int j = 0; j < up_hol.size(); j++){
+                rectangle(frame, up_hol.get(j), new Scalar(0, 255, 0), 2);
+            }
+
+            if (flag == 1 && time.compareTo(LocalTime.now().minusMinutes(2)) < 0) {
+                Imgcodecs.imwrite("src/results/person.jpg", frame);
+                time = LocalTime.now();
             }
 
             imshow("Live", frame);
 
-
-
-            if (waitKey(1) >= 0) {
+            if (waitKey(5) == ' ') {
+                HighGui.destroyAllWindows();
                 break;
 
             }
